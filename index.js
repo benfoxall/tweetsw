c249b7f1c68b375030385e91fd6d6a98c0c182b9
Ben Foxall <ben@whiteoctober.co.uk>
2015-11-24 19:23:22 +0000

    ...and publish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').load();
 
 var Tweets = require('tweets');
+var Pusher = require('pusher');
 
 var stream = new Tweets({
   consumer_key:        process.env.TWITTER_CONSUMER_KEY,
@@ -9,9 +10,17 @@ var stream = new Tweets({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+var pusher = new Pusher({
+  appId:  process.env.PUSHER_APP_ID,
+  key:    process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_SECRET_KEY
+});
+
 
 stream.filter({track: 'pizza'});
 
 stream.on('tweet', function(t){
   console.log("Got a tweet!", t.text);
+
+  pusher.trigger('all', 'tweet', t);
 });
