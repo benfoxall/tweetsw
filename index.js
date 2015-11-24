43d8e1593dc3664b3392c09445cb1ea5408b4778
Ben Foxall <ben@whiteoctober.co.uk>
2015-11-24 20:09:19 +0000

    hardcode built version (dist out of date on 128)

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ stream.on('tweet', function(t){
 var app = express();
 
 app.use(express.static('public'));
-app.use(express.static('node_modules/pusher-js/dist'));
 
 app.get('/key', function(req, res){
     res.send(process.env.PUSHER_APP_KEY);
